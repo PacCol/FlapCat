41cf599a7b1e6d3c5bac2133b3b6fa463294f513
PacCol <pacome.colinet1@gmail.com>
2021-10-09 20:11:10 +0200

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
-apt-get install python3.8-venv
-python3 -m venv venv
-source venv/bin/activate
 cd recognition/
 mkdir dataset
 cd ../
-pip3 install -r requirements.txt
+apt-get install python3-opencv
+pip3 install opencv-python
+pip3 install opencv-contrib-python
+apt-get install libcblas-dev
+apt-get install libhdf5-dev
+apt-get install libhdf5-serial-dev
+apt-get install libatlas-base-dev
+apt-get install libjasper-dev 
+apt-get install libqtgui4 
+apt-get install libqt4-test
+pip3 install flask_sqlalchemy
+pip3 install sqlalchemy
+pip3 install smbus
+pip3 install imutils
 cd static
 rm -rf RealCSS
 git clone https://github.com/PacCol/RealCSS.git
